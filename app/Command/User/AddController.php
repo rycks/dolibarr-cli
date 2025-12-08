b039ffc4ef2eac1aed9299810727ea0d045ba658
Eric Seigne <eric.seigne@cap-rel.fr>
2025-12-08 13:18:43 +0100

    use helper to find admin account

--- app/Command/User/AddController.php
+++ app/Command/User/AddController.php
@@ -6,6 +6,7 @@ namespace App\Command\User;
 
 use Minicli\Command\CommandController;
 use Minicli\Input;
+use App\Util\DolibarrHelper;
 
 class AddController extends CommandController
 {
@@ -87,10 +88,12 @@ Examples:
 
         // Create a user object if not exists (required for user creation)
         if (!isset($user) || !is_object($user)) {
-            require_once DOL_DOCUMENT_ROOT . '/user/class/user.class.php';
-            $user = new \User($db);
-            //TODO admin a modifier
-            $user->fetch('', 'admin');
+            $user = DolibarrHelper::getFirstSuperAdminUser();
+            if (!$user) {
+                $this->error("No super admin user found! Cannot create user.");
+                $this->info("Please ensure at least one active super admin exists.");
+                return;
+            }
         }
 
         // Create new user object
